1dfc4f0f1505e27ecbb291ff8cc5abfbaa817381
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/GetContentInfoExample/Form1.cs
@@ -0,0 +1,42 @@
+﻿#region #Reference
+using System;
+using System.Windows.Forms;
+using DevExpress.Pdf;
+// ...
+#endregion #Reference
+
+
+namespace GetContentInfoExample {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+#region #Code
+private void Form1_Load(object sender, EventArgs e) {
+    this.pdfViewer.LoadDocument(@"..\..\Demo.pdf");
+    this.pdfViewer.ZoomMode = DevExpress.XtraPdfViewer.PdfZoomMode.FitToWidth;
+}
+
+private void pdfViewer_MouseMove(object sender, MouseEventArgs e) {
+    PdfDocumentContent content = pdfViewer.GetContentInfo(e.Location);
+    if (content != null) {
+        string contentTypeText = content.IsSelected ? "Selected " : "Unselected ";
+        switch (content.ContentType) {
+            case PdfDocumentContentType.Text:
+                contentTypeText = contentTypeText + "text";
+                break;
+            case PdfDocumentContentType.Image:
+                contentTypeText = contentTypeText + "image";
+                break;
+            default:
+                contentTypeText = "The content is empty";
+                break;
+        }
+        bsiContentType.Caption = contentTypeText;
+    }
+}
+#endregion #Code
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to determine the PDF content type corresponding to a specific document point
+
+
+<p>This example illustrates how to determine the type of the PDF content corresponding to the document area that is being clicked.</p>
+
+<br/>
+
+
